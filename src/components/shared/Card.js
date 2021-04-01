d59c86c81500c0a396b0f75ceea01e25fd659360
Joan M.O <joanmiquelortega@gmail.com>
2021-04-01 19:17:29 +0200

    change a href to Link, with two options onlick or link

--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -1,32 +1,35 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+//import { useHistory, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Card = ({ adverts }) => {
 
+  /*
   const history = useHistory();
   const showDetail = id => {
      history.push(`/advert/${id}`);
    };
-
+  */
   
-    return (
+  return (
         <div className="row">
           {adverts.map(advert =>
-          <div className="col-4"  key={advert.id}> 
-     
+          <div key={advert.id} className="col-6"> 
+          <Link to={"/advert/"+advert.id}>
             <div className="card" style={{width:"18rem"}}>
             <div className="card-body">
                 <h5 className="card-title">{advert.name}</h5>
                 <p className="card-text">{advert.price}</p>
                 <p className="card-text">{advert.sale === true ? 'venta' : 'compra'}</p>
                 <p className="card-text" style={{fontWeight:"bold"}}>{advert.tags}</p>
-                <button className="btn btn-primary" onClick={()=>showDetail(advert.id)}>Show detail</button>
-            </div>
-            </div>
-            </div>
-            )}
+                <button className="btn btn-primary" /*onClick={()=>showDetail(advert.id)}*/>Show detail</button>
+             </div>
+             </div>   
+           </Link>
           </div>
+          )}
+        </div>
     )
 }
 
